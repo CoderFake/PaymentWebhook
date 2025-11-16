aec8ed52441877ea8d775d171d91ac14360e499a
coderfake <hoangdieu22022002@gmail.com>
2025-11-17 02:57:38 +0700

    Update webhook URL from 'webhook/casso/' to 'webhook/bank-transaction/' for casso_webhook view

--- webhook/urls.py
+++ webhook/urls.py
@@ -5,7 +5,7 @@ app_name = 'webhook'
 
 urlpatterns = [
     path('', views.payment_view, name='payment'),
-    path('webhook/casso/', views.casso_webhook, name='casso_webhook'),
+    path('webhook/bank-transaction/', views.casso_webhook, name='casso_webhook'),
     path('api/payment-status/<str:order_id>/', views.check_payment_status, name='check_payment_status'),
     path('api/payment-info/<str:order_id>/', views.get_payment_info, name='get_payment_info'),
 ]
